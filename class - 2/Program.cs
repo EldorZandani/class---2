e048faae500c8b8ce329b439e667ccc1760f1753
EldorZandani <fox18966@gmail.com>
2022-03-10 15:42:45 +0200

    hot calculate average in two classes

---
+++ class - 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student my_student1 = new student("eldor", 555, 0);
+            my_student1.calculateaverage();
+            Console.WriteLine("your avearge is :{0}", my_student1.average);
+
+            student my_student2 = new student("eldor", 555, 0);
+            my_student2.calculateaverage();
+            Console.WriteLine("your avearge is :{0}", my_student2.average);
+
+            student my_student3 = new student("eldor", 555, 0);
+            my_student3.calculateaverage();
+            Console.WriteLine("your avearge is :{0}", my_student3.average);
+
+        }
+    }
+}
---
+++ class - 2/student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class___2
+{
+    class student
+    {
+        private String fullName;
+
+        public String FullName
+        {
+            get { return fullName; }
+            set { fullName = value; }
+        }
+
+        private int ID;
+
+        public int id
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+        private int Average;
+
+        public int average
+        {
+            get { return Average; }
+            set { Average = value; }
+        }
+
+        public student(string fullname, int id, int average)
+        {
+            this.fullName = fullname;
+            this.id = id;
+            this.average = average;
+        }
+        public void calculateaverage()
+        {
+            int a, b, c, d, e;
+            //for (int i = 0; i > 4; i++)
+            //    Console.WriteLine("please enter student's name");
+            //string fullname = Console.ReadLine();
+            //Console.WriteLine("please enter the student's ID");
+            //int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter 5 grades of the student's");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            average = (a + b + c + d + e) / 5;
+        }
+
+    }
+}
